a9358e23445d360aa53a8675c86c555a5017cc05
Ismail Mohammed <ismailmo4@gmail.com>
2021-09-17 23:47:50 +0100

    added template spot detection via web form

--- formpyapp/api.py
+++ formpyapp/api.py
@@ -1,15 +1,24 @@
 import base64
 import io
+from typing import Tuple
 
 import cv2
 import formpy.utils.img_processing as ip
 import numpy as np
+from formpy.utils.template_definition import find_spots
 from PIL import Image
 
 
-def thresh_img(img: np.array):
-    """test func to render images"""
-    img = Image.fromarray(ip.thresh_img(img))
+def img_to_str(img: np.array) -> str:
+    """writes numpy array img to str
+
+    Args:
+        img (np.array): numpy array representation of image
+
+    Returns:
+        str: base64 string of image
+    """
+    img = Image.fromarray(img)
     img_buf = io.BytesIO()
     img.save(img_buf, "PNG")
     img_buf.seek(0)
@@ -17,7 +26,41 @@ def thresh_img(img: np.array):
     return str(img_base64).split("'")[1]
 
 
-def read_img(img_str):
+def mark_spots(img: np.array) -> Tuple[list[list[int]], np.array]:
+    """marks detected spots on template image
+
+    Args:
+        img (np.array): image returned from read_img
+
+    Returns:
+        np.array: img with bounding boxes around detected spots
+    """
+    processed_img = process_img(img)
+    spot_coords = find_spots(processed_img)
+    color_img = cv2.cvtColor(processed_img, cv2.COLOR_GRAY2BGR)
+    for x, y in spot_coords:
+        cv2.rectangle(
+            color_img, (x - 13, y - 13), (x + 13, y + 13), (255, 0, 0), 2
+        )
+
+    return spot_coords, color_img
+
+
+def process_img(img: np.ndarray):
+    """align and threshold image
+
+    Args:
+        img (np.ndarray): original scan of page
+
+    Returns:
+        np.ndarray: img with applied threshold and alignment
+    """
+    thresh_img = ip.thresh_img(img)
+    aligned_img = ip.align_page(thresh_img)
+    return aligned_img
+
+
+def str_to_img(img_str: str) -> np.array:
     """reads image from web form"""
     img_arr = np.fromstring(img_str, np.uint8)
     img = cv2.imdecode(img_arr, cv2.IMREAD_COLOR)
--- formpyapp/views.py
+++ formpyapp/views.py
@@ -1,9 +1,6 @@
-import numpy as np
-from flask import Flask, render_template, request
-from flask.helpers import url_for
-from werkzeug.utils import redirect
+from flask import Flask, jsonify, render_template, request
 
-from .api import read_img, thresh_img
+from .api import img_to_str, mark_spots, process_img, str_to_img
 
 app = Flask(__name__)
 
@@ -21,9 +18,14 @@ def create_template():
 @app.route("/create", methods=["POST"])
 def handle_template():
     img_str = request.files["uploadedImg"].read()
-    img = read_img(img_str)
-    img_processed = thresh_img(img)
-    return render_template("view_template.html", img=img_processed)
+    img = str_to_img(img_str)
+    img_processed = process_img(img)
+    spot_coords, marked_img = mark_spots(img_processed)
+    marked_img_str = img_to_str(marked_img)
+    num_spots = len(spot_coords)
+    return render_template(
+        "view_template.html", img=marked_img_str, num_spots=num_spots
+    )
 
 
 @app.get("/view")
