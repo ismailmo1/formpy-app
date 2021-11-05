890518e27ddac2f5a4bd3db60cb7fc141c9534fd
Ismail Mohammed <ismailmo4@gmail.com>
2021-11-05 20:22:59 +0000

    create template process steps

--- formpyapp/static/scripts/canvasScript.js
+++ formpyapp/static/scripts/canvasScript.js
@@ -2,22 +2,111 @@
 // add options via object
 const addCircleBtn = document.getElementById("addCircle");
 const exportBtn = document.getElementById("export");
-const canvas = new fabric.Canvas("canvas");
+const defineCanvas = new fabric.Canvas("defineCanvas");
+const alignCanvas = new fabric.Canvas("alignCanvas");
 const zoomIn = document.getElementById("zoomIn")
 const zoomOut = document.getElementById("zoomOut")
 const panMode = document.getElementById("panMode")
 const circleList = document.getElementById("circleList")
-const intNavBtn = document.getElementById("interactiveNavButton");
+const defineNavBtn = document.getElementById("defineNavButton");
+const alignNavBtn = document.getElementById("alignNavButton");
 const canvasContainer = document.querySelector("#canvasDiv")
 const canvasControls = document.querySelector("#canvasControls")
 const addQn = document.getElementById("addQn");
 const canvasControlsList = document.getElementById("canvasControlsList")
 const pickQnMenu = document.getElementById("pickQn")
+const imgForm = document.getElementById("uploadImgForm");
+const uploadTab = document.getElementById("nav-upload");
+const alignTab = document.getElementById("nav-align");
+const alignBtn = document.getElementById("alignNavButton");
+const uploadBtn= document.getElementById("uploadNavButton");
+const activeBtnClassList = ["nav-link", "active"]
+const inactiveBtnClassList = ["nav-link", "disabled"]
+const activeTabClassList = ["tab-pane", "fade", "show", "active"]
+const inactiveTabClassList = ["tab-pane", "fade"]
+let defineCanvasImg = null
+let alignCanvasImg = null
+
+const creationSteps ={
+  UPLOAD:"upload",
+  ALIGN: "align",
+  DEFINE:"define",
+  SAVE:"save"
+}
+
+// create template detection
+imgForm ? creating = true : creating=false
+// image upload events if on create template page creating template
+if(creating){
+  // we are on the create template page
+  imgForm.addEventListener("submit", async (e) => {
+    // prevent form post request
+    e.preventDefault();
+    // save image upload field to add as hidden field to new question definition form
+    alignCanvasImg=await alignImg(imgForm);
+    prepareAlignTab(alignCanvasImg);
+})}else{
+  // we are on the edit template page
+
+};
+
+// ajax form post to align template image
+async function alignImg(form) {
+  let formData = new FormData(form);
+  res = await fetch("/align-template", { method: "POST", body: formData });
+  let {img} = await res.json();
+  return img;
+}
+
+function prepareAlignTab(alignedImgData){
+  deactivateTab(creationSteps.UPLOAD);
+  activateTab(creationSteps.ALIGN);
+  prepareCanvas(alignCanvas, alignedImgData)
+}
+
+function getStepEl(creationStep){
+  let tabId = `nav-${creationStep}`
+  let buttonId = `${creationStep}NavButton`
+  const tab = document.getElementById(tabId)
+  const button = document.getElementById(buttonId)
+  return {"tab": tab,"button":button}
+}
+
+function activateTab(creationStep){
+  const {tab, button} = getStepEl(creationStep)
+  // clear current classes
+  button.classList.remove(...button.classList)
+  button.classList.add("nav-link", "active")
+  tab.classList.remove(...tab.classList)
+  tab.classList.add("tab-pane", "fade", "show", "active")
+
+}
+
+function deactivateTab(creationStep){
+  const {tab, button} = getStepEl(creationStep)
+  button.classList.remove(...button.classList)
+  button.classList.add("nav-link", "disabled")
+  tab.classList.remove(...tab.classList)
+  tab.classList.add("tab-pane", "fade", "disabled")
+}
+
+
+
 let currentQuestion = null
 // hold spots data
 let questions = {}
 
-function setCurrentQuestion(qnNumber){  
+window.addEventListener("resize", resizeCanvas)
+// ajax form post to align template image
+async function alignImg(form) {
+    let formData = new FormData(form);
+    res = await fetch("/align-template", { method: "POST", body: formData });
+    let {img} = await res.json();
+    return img;
+  }
+  
+function setCurrentQuestion(qnNumber){
+    // globally set current question  
     currentQuestion = qnNumber
     const badge = document.createElement("span")
     badge.classList.add("position-absolute", "top-0", "start-100", "translate-middle", 
@@ -38,7 +127,6 @@ addQn.addEventListener("click",(e)=>{
     span.innerText = canvasControlsList.childElementCount
     
     newQn.addEventListener("click", ()=>{
-        console.log(newQn.innerText)
         currentQuestion = newQn.innerText
         canvasControlsList.querySelectorAll(".bg-danger").forEach(span=>{
             span.classList.add("bg-secondary")
@@ -52,12 +140,9 @@ addQn.addEventListener("click",(e)=>{
     canvasControlsList.appendChild(newQn)
     questions[newQn.innerText] = []
 })
-window.addEventListener("resize", resizeCanvas)
 
-async function addImageToCanvas(){
-    let templateImg = document.getElementById("templateImg")
-    
-      const bgImage = new fabric.Image.fromURL(templateImg.src, (img)=>{
+async function addImageToCanvas(canvas, imgData){
+      const bgImage = new fabric.Image.fromURL(imgData, (img)=>{
   
         // get image aspect ratio to decide if scale to height or width
         imgAR = img.width/img.height
@@ -78,138 +163,146 @@ async function addImageToCanvas(){
         
   
   }
+function prepareCanvas(canvas, imgData){
+  let w = canvasContainer.offsetWidth 
+  if(w!=canvas.width){
+      resizeCanvas(canvas, imgData);
+  }
+  addCanvasEventListeners(defineCanvas)
+}
 
 // fire event on bootstrap nav activation (div doesnt have any width until shown!) 
-intNavBtn.addEventListener("shown.bs.tab", ()=>{
-    let w = canvasContainer.offsetWidth 
-    if(w!=canvas.width){
-        resizeCanvas();
-    }
+defineNavBtn.addEventListener("shown.bs.tab", ()=>{
+    prepareCanvas(defineCanvas, defineCanvasImg)
+})
+alignNavBtn.addEventListener("shown.bs.tab", ()=>{
+
+    prepareCanvas(alignCanvas, alignCanvasImg)
+
 })
 // exportBtn.addEventListener('click', resizeCanvas, false);
 
-function resizeCanvas() {
-    console.log("resizing")
+function resizeCanvas(canvas, imgData) {
     canvas.setHeight(canvasContainer.offsetWidth*0.6);
     canvas.setWidth(canvasContainer.offsetWidth);
     let newTop = canvasContainer.offsetTop + (canvasContainer.offsetHeight/2) - (canvasControls.offsetHeight/2)
     canvasControls.setAttribute("style", `left:${canvasContainer.offsetLeft}px;top:${newTop}px`)
-    addImageToCanvas();
+    addImageToCanvas(canvas,imgData);
 
     canvas.renderAll();
 }
 
 
+function addCanvasEventListeners(canvas){
 
-panMode.addEventListener("click", (e)=>{
-    if (!panMode.status){
-        e.target.classList.toggle("panModeOn")
-        panMode.status = true;
-        canvas.isSelection=false
-    }else{
-        e.target.classList.toggle("panModeOn")
+    panMode.addEventListener("click", (e)=>{
+        if (!panMode.status){
+            e.target.classList.toggle("panModeOn")
+            panMode.status = true;
+            canvas.isSelection=false
+        }else{
+            e.target.classList.toggle("panModeOn")
 
-        panMode.status = false;
-        canvas.isSelection=true
-    }}
-)
+            panMode.status = false;
+            canvas.isSelection=true
+        }}
+    )
 
-canvas.on('mouse:down', function(opt) {
-    let evt = opt.e;
-    if (panMode.status) {
-      this.isDragging = true;
-      this.selection = false;
-      this.lastPosX = evt.clientX;
-      this.lastPosY = evt.clientY;
-    }
-  });
-
-  canvas.on('mouse:move', function(opt) {
-    if (this.isDragging) {
-        
-        let e = opt.e;
-        let vpt = this.viewportTransform;
-        // move viewport by amount moved since last move      
-        vpt[4] += e.clientX - this.lastPosX;
-        vpt[5] += e.clientY - this.lastPosY;
-        this.requestRenderAll();
-        this.lastPosX = e.clientX;
-        this.lastPosY = e.clientY;
-    }
-  });
-  canvas.on('mouse:up', function(opt) {
-    // on mouse up we want to recalculate new interaction
-    // for all objects, so we call setViewportTransform
-    this.setViewportTransform(this.viewportTransform);
-    this.isDragging = false;
-    this.selection = true;
-  });
-      
-
-
-// add zoom in/out func
-zoomOut.addEventListener("click", ()=>{
-    canvas.setZoom(canvas.getZoom() *0.9)
-})
-
-zoomIn.addEventListener("click", ()=>{
-    canvas.setZoom(canvas.getZoom() *1.1)
-})
-
-
-exportBtn.addEventListener("click", ()=>{
-    circleList.innerHTML = ""
-    let table = document.createElement("table")
-    let hdRow = document.createElement("tr")
-    let idxHdr = document.createElement("th")
-    let xHdr = document.createElement("th")
-    let yHdr = document.createElement("th")
-    idxHdr.innerText = "Answer No."
-    xHdr.innerText = "x coord"
-    yHdr.innerText =  "y coord"
-    hdRow.appendChild(idxHdr)
-    hdRow.appendChild(xHdr)
-    hdRow.appendChild(yHdr)
-    table.appendChild(hdRow)
-    circleList.appendChild(table)
-
-    canvas.toObject().objects.forEach((obj, i) => {        
-        let row = document.createElement("tr")
-        let ansCol = document.createElement("td")
-        let ansX = document.createElement("td")
-        let ansY = document.createElement("td")
-        ansCol.innerText = i
-        ansX.innerText = Math.round(obj.left/scaleFactor)
-        ansY.innerText = Math.round(obj.top/scaleFactor)
-        row.appendChild(ansCol)
-        row.appendChild(ansX)
-        row.appendChild(ansY)
-        table.appendChild(row)
-
+    canvas.on('mouse:down', function(opt) {
+        let evt = opt.e;
+        if (panMode.status) {
+        this.isDragging = true;
+        this.selection = false;
+        this.lastPosX = evt.clientX;
+        this.lastPosY = evt.clientY;
+        }
     });
-})
-addCircleBtn.addEventListener("click", addCircle)
 
-// get object location when moved
-canvas.on("object:moved", (e)=>{
-    let {type,top, left} = e.target  
-    console.log(type,top/scaleFactor, left/scaleFactor)
-})
+    canvas.on('mouse:move', function(opt) {
+        if (this.isDragging) {
+            
+            let e = opt.e;
+            let vpt = this.viewportTransform;
+            // move viewport by amount moved since last move      
+            vpt[4] += e.clientX - this.lastPosX;
+            vpt[5] += e.clientY - this.lastPosY;
+            this.requestRenderAll();
+            this.lastPosX = e.clientX;
+            this.lastPosY = e.clientY;
+        }
+    });
+    canvas.on('mouse:up', function(opt) {
+        // on mouse up we want to recalculate new interaction
+        // for all objects, so we call setViewportTransform
+        this.setViewportTransform(this.viewportTransform);
+        this.isDragging = false;
+        this.selection = true;
+    });
+        
 
 
-function addCircle(){
-    const circle = new fabric.Circle({
-        radius: 25,
-        top: 200,
-        left: 200,
-        strokeWidth:5,
-        stroke:'rgba(100,100,150, 0.5)',
-        fill:'rgba(0,0,0,0)'
+    // add zoom in/out func
+    zoomOut.addEventListener("click", ()=>{
+        canvas.setZoom(canvas.getZoom() *0.9)
+    })
+
+    zoomIn.addEventListener("click", ()=>{
+        canvas.setZoom(canvas.getZoom() *1.1)
+    })
+
+
+
+    exportBtn.addEventListener("click", ()=>{
+        circleList.innerHTML = ""
+        let table = document.createElement("table")
+        let hdRow = document.createElement("tr")
+        let idxHdr = document.createElement("th")
+        let xHdr = document.createElement("th")
+        let yHdr = document.createElement("th")
+        idxHdr.innerText = "Answer No."
+        xHdr.innerText = "x coord"
+        yHdr.innerText =  "y coord"
+        hdRow.appendChild(idxHdr)
+        hdRow.appendChild(xHdr)
+        hdRow.appendChild(yHdr)
+        table.appendChild(hdRow)
+        circleList.appendChild(table)
+
+        canvas.toObject().objects.forEach((obj, i) => {        
+            let row = document.createElement("tr")
+            let ansCol = document.createElement("td")
+            let ansX = document.createElement("td")
+            let ansY = document.createElement("td")
+            ansCol.innerText = i
+            ansX.innerText = Math.round(obj.left/scaleFactor)
+            ansY.innerText = Math.round(obj.top/scaleFactor)
+            row.appendChild(ansCol)
+            row.appendChild(ansX)
+            row.appendChild(ansY)
+            table.appendChild(row)
+
         });
-    canvas.add(circle);
-    console.log(circle)
-    questions[currentQuestion].push(circle)    
-}
+    })
+    addCircleBtn.addEventListener("click", addCircle)
 
-  
+    // get object location when moved
+    canvas.on("object:moved", (e)=>{
+        let {type,top, left} = e.target  
+    })
+
+
+    function addCircle(){
+        const circle = new fabric.Circle({
+            radius: 25,
+            top: 200,
+            left: 200,
+            strokeWidth:5,
+            stroke:'rgba(100,100,150, 0.5)',
+            fill:'rgba(0,0,0,0)'
+            });
+        canvas.add(circle);
+        questions[currentQuestion].push(circle)    
+    }
+
+}  
  
