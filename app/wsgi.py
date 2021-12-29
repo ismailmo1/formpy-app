79e9242371ad9bf6925ca2d90e133e3789bbc0ef
ismail <ismailmo4@gmail.com>
2021-12-29 21:00:19 +0000

    removed secrets

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,3 +1,8 @@
+from dotenv import dotenv_values, load_dotenv
+
+# load env before importing app
+load_dotenv()
+print(dotenv_values())
 from app import app as application
 
 if __name__ == "__main__":
---
+++ mongo-init.sh
@@ -0,0 +1,10 @@
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+    var rootUser = '$(cat "$MONGO_INITDB_ROOT_USERNAME_FILE")';
+    var rootPassword = '$(cat "$MONGO_INITDB_ROOT_PASSWORD_FILE")';
+    var admin = db.getSiblingDB('admin');
+    admin.auth(rootUser, rootPassword);
+
+    var user = '$(cat "$MONGO_INITDB_USERNAME_FILE")';
+    var passwd = '$(cat "$MONGO_INITDB_PASSWORD_FILE")';
+    db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
+EOF
