3f5e733587f3d6e522dabb691772cd0009db7ff8
ismail <ismailmo4@gmail.com>
2021-12-30 19:39:09 +0000

    fixed nginx conf

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # formpy-app
+
 Web app to open access to formpy
 see https://github.com/ismailmo1/formpy for original library
+
+To use locally:
+
+clone this repo
+
+git clone
+
+add secrets folders and files
+
+mkdir secrets
+touch mongo_root_pwd.txt mongo_pwd.txt mongo_root_user.txt mongo_user.txt
+
+add .env file and and environment variables
+
+touch app/.env
+add FLASK_SECRET environment variable
+FLASK_SECRET=secret
+MONGODB_USERNAME=user
+MONGODB_PASSWORD=pass
--- mongo-init.sh
+++ mongo-init.sh
@@ -1,4 +1,4 @@
-mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+mongosh -- "$MONGO_INITDB_DATABASE" <<EOF
     var rootUser = '$(cat "$MONGO_INITDB_ROOT_USERNAME_FILE")';
     var rootPassword = '$(cat "$MONGO_INITDB_ROOT_PASSWORD_FILE")';
     var admin = db.getSiblingDB('admin');
@@ -6,5 +6,6 @@ mongo -- "$MONGO_INITDB_DATABASE" <<EOF
 
     var user = '$(cat "$MONGO_INITDB_USERNAME_FILE")';
     var passwd = '$(cat "$MONGO_INITDB_PASSWORD_FILE")';
-    db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
+    var dbName = "$MONGO_INITDB_DATABASE"
+    db.createUser({user: user, pwd: passwd, roles: [{role: "readWrite", db:dbName}]});
 EOF
