562ca5f83c5c7815ccaa929035d70ec4aafe9ec5
Ismail Mohammed <ismailmo4@gmail.com>
2022-04-27 15:47:35 +0000

    fixed imports

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -30,7 +30,7 @@ def test_new_coordinate():
 
 
 def test_new_answer():
-    from formpyapp.db.models import Answer, Coordinate2D
+    from app.formpyapp.db.models import Answer, Coordinate2D
 
     coordinates = Coordinate2D(x_coordinate=200, y_coordinate=300)
     answer = Answer(coordinates=coordinates, value="test answer")
@@ -41,7 +41,7 @@ def test_new_answer():
 
 
 def test_new_question():
-    from formpyapp.db.models import Answer, Coordinate2D, Question
+    from app.formpyapp.db.models import Answer, Coordinate2D, Question
 
     # create a bunch of answers
     answers = []
@@ -66,7 +66,7 @@ def test_new_question():
 
 
 def test_new_template():
-    from formpyapp.db.models import (
+    from app.formpyapp.db.models import (
         Answer,
         Coordinate2D,
         Question,
