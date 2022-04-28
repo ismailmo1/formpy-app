546ade9d449dc252d63aded0845461e06677ca5f
Ismail Mohammed <ismailmo4@gmail.com>
2022-04-28 01:57:42 +0000

    cleaned file structure

--- app/app.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from formpyapp import app
-
-if __name__ == "__main__":
-    ENVIRONMENT_DEBUG = os.environ.get("FLASK_DEBUG", True)
-    ENVIRONMENT_PORT = os.environ.get("FLASK_PORT", 5000)
-    app.run(host="0.0.0.0", port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
