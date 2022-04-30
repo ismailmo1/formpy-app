1908f1fe4c68c97fa0ef4f0721e82edda3593cb1
Ismail Mohammed <ismailmo4@gmail.com>
2022-04-30 02:54:51 +0000

    document FileStorage param in read_form_img

--- app/formpyapp/api/img_proc.py
+++ app/formpyapp/api/img_proc.py
@@ -33,7 +33,7 @@ def str_to_img(img_str: str) -> np.array:
 
 
 def read_form_img(img_data: bytes) -> np.array:
-    """reads image from web form"""
+    """reads image from web form FileStorage.read()"""
     img_arr = np.fromstring(img_data, np.uint8)
     img = cv2.imdecode(img_arr, cv2.IMREAD_COLOR)
     return img
