986fd931f5e35e79edd80a96fa37c6e037b4992c
Ismail Mohammed <ismailmo4@gmail.com>
2022-05-01 04:20:36 +0000

    fixed import

--- tests/functional/test_db.py
+++ tests/functional/test_db.py
@@ -41,7 +41,7 @@ def test_update_template(db_user, db_template):
 
 def test_remove_template(db_user, db_template):
     from app.formpyapp.db.models import Template
-    from app.formpyapp.db.utils import remove_template, save_template
+    from app.formpyapp.db.utils import remove_template
 
     template_id = db_template.id
     remove_template(template_id)
